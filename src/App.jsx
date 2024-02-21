aa1b172becc1b009c161779346137f39545510fe
Mitch Wells <mitchwells@Mitchs-iMac.local>
2024-02-21 15:36:37 -0600

    more stuff

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [selectedContactId, setSelectedContactId] = useState(null)
-
+console.log(selectedContactId)
   return (
     <>
-    {selectedContactId ? (<div>You selected: <SelectedContact /></div>) : <ContactList setSelectedContactId={setSelectedContactId} />} 
+    {selectedContactId ? (<div>You selected: <SelectedContact selectedContactId={selectedContactId} /></div>) : <ContactList setSelectedContactId={setSelectedContactId} />} 
     </>
   )
 }
--- src/Components/SelectedContact.jsx
+++ src/Components/SelectedContact.jsx
@@ -1,10 +1,24 @@
 import React from "react"
+import { useState } from "react"
+import { useEffect } from "react"
 
-function SelectedContact({  }){
+
+
+function SelectedContact({ selectedContactId, setSelectedContactId }){
+    const [contact, setContact] = useState([null]);
+
+   useEffect(){
+
+   },[]
+    
+   
+
+
+
 return (
     <div></div>
 )
-
 }
+
 
 export default SelectedContact
